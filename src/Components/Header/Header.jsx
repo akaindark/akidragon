e0b1cd2602ab7d1a60c80bdd8a2b180ed73405c0
akaindark <akaindark@gmail.com>
2021-03-30 19:12:23 +0300

    fix style header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import header from './Header.module.css';
 import Menu from "./Menu/Menu";
 import Logo from "../Logo/Logo";
+import Lang from "./Lang/Lang";
 
 const Header = () => {
   return(
-    <header className="header.header">
-      <Logo/>
-      <Menu/>
+    <header className={header.header}>
+      <Logo />
+      <Menu />
+      <Lang />
     </header>
   );
 }
